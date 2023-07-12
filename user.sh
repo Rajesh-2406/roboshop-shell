668cee2bbd2c263bca38b3969cdb424d521d38e4
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-12 08:10:17 +0530

    user script

--- user.sh
+++ user.sh
@@ -1,41 +1,42 @@
-echo -e "\e[33mConfiguring Nodejs Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+source common.sh
+echo -e "${color} Configuring Nodejs Repos${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_file
 
-echo -e "\e[31m Install Nodejs\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+echo -e "${color} Install Nodejs ${nocolor}"
+yum install nodejs -y &>>log_file
 
-echo -e "\e[33mAdd Application User\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e " ${color}Add Application User ${nocolor}"
+useradd  &>>log_file
 
-echo -e "\e[33m Create Application Diretectory \e[0m"
-rm -rf /app &>>/tmp/roboshop.log
+echo -e "${color} Create Application Diretectory ${nocolor}"
+rm -rf /app &>>log_file
 mkdir /app
 
-echo -e "\e[33mDownload Application Content\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+echo -e "${color} Download Application Content ${nocolor}"
+curl -o //user.zip https://-artifacts.s3.amazonaws.com/user.zip &>>log_file
 cd /app
 
-echo -e "\e[33m Extract Application Content\e[0m"
-unzip /tmp/user.zip
+echo -e "${color} Extract Application Content ${nocolor}"
+unzip //user.zip &>>log_file
 cd /app
 
-echo -e "\e[33m Install NodeJS Dependencies\e[0m"
-npm install
+echo -e "${color} Install NodeJS Dependencies ${nocolor}"
+npm install &>>log_file
 
 
-echo -e "\e[33m Setup SystemD Service \e[0m"
+echo -e "${color}Setup SystemD Service ${nocolor}"
 cp user.service /etc/systemd/system/user.service
 
-echo -e "\e[33m Start User Service\e[0m"
+echo -e "${color} Start User Service ${nocolor}"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
 
-echo -e "\e[33m Copy mongoDB Repo file \e[0m"
+echo -e "\e[ Copy mongoDB Repo file \e[${nocolor}"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
-echo -e "\e[33m Install MOngoDB Client \3[0m"
+echo -e "\e[ Install MOngoDB Client \3[${nocolor}"
 yum install mongodb-org-shell -y
 
-echo -e "\e[33m Load Schema \e[0m"
+echo -e "\e[ Load Schema \e[${nocolor}"
 mongo --host mongodb.devops2406.store </app/schema/user.js
