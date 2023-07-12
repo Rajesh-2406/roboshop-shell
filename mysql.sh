ba58e78880fe228a3c57f3614f53f9f3ea77380e
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-12 07:32:07 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -2,22 +2,22 @@
 
 
 echo -e "${color}Disable Mysql Default Version ${nocolor}"
-yum module disable mysql -y &>>/tmp/roboshop.log
+yum module disable mysql -y &>>log_file
 stat_check $?
 
 echo -e "${color} Copy Mysql repo file ${nocolor}"
-cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repo.d/mysql.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repo.d/mysql.repo &>>log_file
 stat_check $?
 
 echo -e "${color} Install Mysql Community Server ${nocolor}"
-yum install mysql-community-server -y &>>/tmp/roboshop.log
+yum install mysql-community-server -y &>>log_file
 stat_check $?
 
 echo -e "${color} Start Mysql Service ${nocolor}"
-systemctl enable mysqld &>>/tmp/roboshop.log
-systemctl start mysqld &>>/tmp/roboshop.log
+systemctl enable mysqld &>>log_file
+systemctl start mysqld &>>log_file
  stat_check $?
 
 echo -e "${color} Setup Mysql Password ${nocolor}"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+mysql_secure_installation --set-root-pass RoboShop@1 &>>log_file
 stat_check $?
